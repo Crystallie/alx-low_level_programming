3e05f3ba61ce58c9109d0e03f45d2c0858db1618
Oyindamola Olupitan <olupitanoyin114@gmail.com>
2022-03-20 10:50:40 -0700

    print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,8 +3,8 @@
 /**
  * main - This program prints all possible combinations of
  * single digit numbers
- * Numbers are separated by a comma printed in ascending order,
- * and is printed with putchar only
+ * Numbers are separated by a comma, printed in ascending order,
+ * and is printed with putchar only.
  * Return: Always 0 (Success)
  */
 int main(void)
